00baa1bb9e011f0eb2e4fa9ca751036ac547dd3c
L Stephens <lstephens46@my.stlcc.edu>
2019-04-11 14:35:21 -0500

    finish turtles pass all tests

--- Turtles/Program.cs
+++ Turtles/Program.cs
@@ -7,7 +7,13 @@ namespace Turtles
         public static void Main()
         {
             // TODO: Prints "I like turtles." 1000 times.
-
+             
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    Console.WriteLine("I like turtles.");
+                }
+            }
             Console.ReadLine();
         }
     }
